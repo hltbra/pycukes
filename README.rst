ff396bf2ea96710f8bf0f73d3e89ce4fec6e76fb
Hugo Lopes Tavares <hltbra@gmail.com>
2009-08-18 11:05:34 -0300

    added README.rst and classifiers to setup.py

---
+++ README.rst
@@ -0,0 +1,10 @@
+PyCukes
+=======
+
+PyCukes is a Cucumber-like BDD framework built on top of Pyhistorian.
+PyCukes was created to fill the gap pyhistorian left, so with it is possible to talk to your stakeholders first with text files, instead of simple understendable python files like Pyhistorian.
+
+
+What need to be done
+====================
+There is a simple StoryRunner, but there is no console to stakeholders/end-users run the stories.
--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ version = '0.1'
 
 setup(name='pycukes',
       version=version,
-      description="A text-style BDD framework built on top of Pyhistorian",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="A Cucumber-like BDD framework built on top of Pyhistorian",
+      long_description=open('README.rst').read(),
+      classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Software Development :: Testing',],
       keywords='bdd behaviour behavior pyhistorian story',
       author='Hugo Lopes Tavares',
       author_email='hltbra@gmail.com',
       url='http://github.com/hugobr/pycukes',
-      license='MIT',
+      license='MIT License',
       packages=['pycukes', 'pycukes.tests', 'pycukes.tests.scenarios',],
       package_dir={'pycukes': 'src',  'pycukes.tests': 'src/tests', 'pycukes.tests.scenarios': 'src/tests/scenarios'},
       include_package_data=True,
