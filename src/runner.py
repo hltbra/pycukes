5d2f1825206514a74e37639fa55c7a9ba1f44076
Hugo Lopes Tavares <hltbra@gmail.com>
2009-08-18 01:39:06 -0300

    multiple scenarios working!

--- src/runner.py
+++ src/runner.py
@@ -40,6 +40,7 @@ class StoryRunner(object):
                      'scenarios': [],})
 
     def run(self):
+#        import pdb;pdb.set_trace()
         scenarios = self._parsed_story.get_scenarios()
         for scenario_title, steps in scenarios:
             new_scenario = type('NewScenario',
@@ -49,22 +50,27 @@ class StoryRunner(object):
                                 '_whens': [],
                                 '_thens': [],
                                 })
-            for given, value in self._all_givens.items():
-                new_scenario._givens.append( (value[0], given, value[1]) )
-            for when, value in self._all_whens.items():
-                new_scenario._whens.append( (value[0], when, value[1]) )
-            for then, value in self._all_thens.items():
-                new_scenario._thens.append( (value[0], then, value[1]) )
+            for given_message in steps['given']:
+                if given_message not in self._all_givens:
+                    new_scenario._givens.append( (None, given_message, ()) )
+                for given_regex, value in self._all_givens.items():
+                    if given_message == given_regex:
+                        new_scenario._givens.append( (value[0], given_message, value[1]) )
+            
+            for when_message in steps['when']:
+                if when_message not in self._all_whens:
+                    new_scenario._whens.append( (None, when_message, ()) )
+                for when_regex, value in self._all_whens.items():
+                    if when_regex == when_message:
+                        new_scenario._whens.append( (value[0], when_message, value[1]) )
 
-            for given in steps['given']:
-                if given not in self._all_givens:
-                    new_scenario._givens.append( (None, given, ()) )
-            for when in steps['when']:
-                if when not in self._all_whens:
-                    new_scenario._whens.append( (None, when, ()) )
-            for then in steps['then']:
-                if then not in self._all_thens:
-                    new_scenario._thens.append( (None, then, ()) )
+            for then_message in steps['then']:
+                if then_message not in self._all_thens:
+                    new_scenario._thens.append( (None, then_message, ()) )
+                for then_regex, value in self._all_thens.items():
+                    if then_regex == then_message:
+                        new_scenario._thens.append( (value[0], then_message, value[1]) )
+
  
             self._pycukes_story.scenarios.append(new_scenario)
         self._pycukes_story.run()
